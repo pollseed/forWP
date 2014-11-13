1f5097817b499a4e1b0376f39237594b02cd1f75
Seed <cb.so.devope+0001@gmail.com>
2014-11-13 23:11:55 +0900

    Create sample1.py

---
+++ python/sample1.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+# 事前にx, yを定義
+plt.scatter(x,y)
+
+# ただのタイトル
+plt.title("Sample")
+
+# X軸のラベル
+plt.xlabel("X-label")
+
+# Y軸のラベル
+plt.ylabel("Y-lael")
+
+# グリッドを付ける
+plt.grid()
+
+# 出力
+plt.show()
